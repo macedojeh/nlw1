f960674a276bbae5df2059ff23f3dae4fe878c5d
Jessica Macedo <jessicaamacedo@hotmail.com>
2020-06-02 22:25:10 -0300

    att

---
+++ scripts/intro.js
@@ -0,0 +1,73 @@
+// variaveis, tipos de dados
+// let myvar = "He" pode usar let ou var
+//const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+// "Isso eh uma string"
+// 'Isso tambem eh uma string'
+// `Isso eh uma string tambem`
+
+// number - ele mesmo reconhece que eh numero e nao letra, entao ele mesmo soma, mas se por uma das aspas ele vai achar que eh letra e vai repetir.
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+
+// Estrutura de Dados
+// objeto - possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+     // return "run Forest"
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// document.write(pessoa.altura)
+// document.write(pessoa.idade)
+// document.write(pessoa.solteiro)
+// document.write()
+// document.write(pessoa.correr())
+
+// pessoa.correr()
+
+// Array - Vetores = colecao de dados
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funcoes - Funcionalidades - Atalhos - Reuso de codigo
+// registrar funcao
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// execucao de funcao
+// sayMyName("Jessica")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+// Condicionais
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   // caminho 1 
+//   document.write("Reprovado")
+// } else {
+//   // caminho 2
+//   document.write("Aprovado")
+// }
+
+// Loop - Repeticoes
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
